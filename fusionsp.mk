8a36456fd530d1e4c43c4237389c64299ad652d2
srisurya95 <srisurya95@gmail.com>
2015-08-19 15:30:51 +0000

    Make it a Fusion Device.

---
+++ fusionsp.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/motorola/falcon/full_falcon.mk)
+
+# Inherit some common Fusion stuff.
+$(call inherit-product, vendor/fusion/config/common_phone.mk)
+
+PRODUCT_RELEASE_NAME := MOTO G
+PRODUCT_NAME := falcon
